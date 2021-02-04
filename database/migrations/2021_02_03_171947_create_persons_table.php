7a70cea3b96e92c85c76cf8468445e1f5d97f309
Vinícius Moreira <vinicius.vsm777@gmail.com>
2021-02-04 14:13:53 -0300

    table person migration success

---
+++ database/migrations/2021_02_03_171947_create_persons_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePersonsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('persons', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            
+            $table->string('name');
+            $table->date('birth');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('persons');
+    }
+}
