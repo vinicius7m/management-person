c56bca043499856981f304af4efba6e2444a46e3
Vinícius Moreira <vinicius.vsm777@gmail.com>
2021-02-10 17:59:53 -0300

    Refactoring form create

--- resources/views/people/create.blade.php
+++ resources/views/people/create.blade.php
@@ -1,23 +1,29 @@
 @extends('layouts.app')
 
 @section('content')
-<h1>Criar loja</h1>
+<h1 class="mt-5">Criar loja</h1>
 
 <form action="{{ route('people.person') }}" method="POST">
 
     <input type="hidden" name="_token" value="{{ csrf_token() }}">
 
-    <div>
+    <div class="form-group"> 
         <label for="name">Nome: </label>
-        <input type="text" name="name" id="name" />
+
+        <div>
+            <input type="text" name="name" id="name" class="form-control" required />
+        </div>
     </div>
-    <div>
+    <div class="form-group">
         <label for="birth">Nascimento</label>
-        <input type="date" name="birth" id="birth" />
+
+        <div>
+            <input type="date" name="birth" id="birth" class="form-control" required />
+        </div>
     </div>
 
     <div>
-        <button type="submit">Enviar</button>
+        <button class="btn btn-primary btn-block" type="submit">Enviar</button>
     </div>
 </form>
 @endsection
